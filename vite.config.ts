1fd05f50634d82abfc213714b82ce40be413d8dd
Станислав <103207960+LuministRU@users.noreply.github.com>
2026-04-01 21:45:44 +0300

    domen

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mysitehooperstyle.github.io/',
+  base: '/',
   plugins: [inspectAttr(), react()],
   resolve: {
     alias: {
