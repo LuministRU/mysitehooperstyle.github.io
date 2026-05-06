34ee226c150f5dbec762e9372b55b2b3fbab6ed2
Станислав <103207960+LuministRU@users.noreply.github.com>
2026-05-06 23:22:42 +0300

    4

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -132,7 +132,7 @@ export function Hero() {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 1.6, duration: 0.6 }}
-          className="absolute bottom-2 left-1/2 -translate-x-1/2"
+          className="absolute bottom-1 left-1/2 -translate-x-1/2"
         >
           <motion.div
             animate={{ y: [0, 8, 0] }}
